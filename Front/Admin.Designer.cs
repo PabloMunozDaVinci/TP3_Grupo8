e5f64e8c80cca582fdca700afa9d7265e26a26c9
Franco de San Vicente <francodv31@hotmail.com>
2022-06-24 21:21:03 -0300

    Modificar usuario desde form admin

--- Front/Admin.Designer.cs
+++ Front/Admin.Designer.cs
@@ -192,6 +192,7 @@
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Admin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Admin";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
